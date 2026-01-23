276c6e766a5d450d1c530c248a99ac481b6e99dc
Tamjidul-Islam <tamjidtj6@gmail.com>
2026-01-23 13:09:04 +0600

    Lab 8 Set Up React Frontend for Task Display

--- Lab_7/task-api-lab/src/app.js
+++ Lab_7/task-api-lab/src/app.js
@@ -1,18 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const taskRoutes = require('./routes/taskRoutes');
 const errorHandler = require('./middlewares/errorHandler');
 const AppError = require('./utils/AppError');
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
 app.use(express.json());
 
 // API routes
 app.use('/api/v1/tasks', taskRoutes);
 
-// 404 handler (EXPRESS 5 SAFE)
+// 404 handler (Express 5 safe)
 app.use((req, res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 // Global error handler
